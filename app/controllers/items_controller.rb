85b797a0c0b265a667225258bf5cbd721449ceff
kazuyama-4 <howholic.royalky420@gmail.com>
2021-05-10 19:03:12 +0900

    controller設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
-  def index; end
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
 
   def new
     @item = Item.new
