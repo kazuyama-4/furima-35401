cc3a6e4bb1fb24864b22452d9df22fc1fffe0ef3
kazuyama-4 <howholic.royalky420@gmail.com>
2021-05-13 14:53:46 +0900

    修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,10 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: %i[index]
-  before_action :move_to_index, only: %i[index]
   before_action :set_item, only: %i[index create]
+
   def index
     @order = Order.new
+    　redirect_to root_path if current_user == @item.user_id || @item.purchase.present?
   end
 
   def create
@@ -20,25 +21,21 @@ class PurchasesController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:postal, :prefecture_id, :city, :house_number, :building_name, :call_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order).permit(:postal, :prefecture_id, :city, :house_number, :building_name, :call_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
 
-  def move_to_index
-    if current_user == @item.user || @item.purchase.present?
-      　redirect_to root_path
-  end
-  
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: Item.find(params[:item_id])[:price],
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id])[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
-
 end
