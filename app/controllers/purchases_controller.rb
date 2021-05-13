f8132e8dd9e657a85287f3a7f6274f5d82bae469
kazuyama-4 <howholic.royalky420@gmail.com>
2021-05-13 14:40:28 +0900

    controller設定

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,44 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: %i[index]
+  before_action :move_to_index, only: %i[index]
+  before_action :set_item, only: %i[index create]
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal, :prefecture_id, :city, :house_number, :building_name, :call_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user == @item.user || @item.purchase.present?
+      　redirect_to root_path
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Item.find(params[:item_id])[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+end
